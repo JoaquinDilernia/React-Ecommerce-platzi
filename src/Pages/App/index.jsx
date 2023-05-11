36554227144f374e205a5071a4e4413b28ffedff
joacodiler <jdilernia99@gmail.com>
2023-05-11 17:13:58 -0300

    first commit

---
+++ src/Pages/App/index.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-red-100'>hola mundo</h1>
+    </>
+  )
+}
+
+export default App
